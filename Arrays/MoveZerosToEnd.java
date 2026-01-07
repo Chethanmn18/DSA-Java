dbb7c46bfc23de8ca884da65d82c530c00c7ed77
Chethanmn18 <chethan18mn@gmail.com>
2026-01-07 22:12:58 +0530

    DSA

--- Arrays/MoveZerosToEnd.java
+++ Arrays/MoveZerosToEnd.java
@@ -7,7 +7,7 @@ public class MoveZerosToEnd  extends AlgorithmTemplate{
     @Override
     public void better(int[] arr) {
         // TODO Auto-generated method stub
-        
+        System.out.println("Beter solution doesn't exist for this one.");
     }
 
     @Override
@@ -36,7 +36,31 @@ public class MoveZerosToEnd  extends AlgorithmTemplate{
     @Override
     public void optimal(int[] arr) {
         // TODO Auto-generated method stub
+        int n = arr.length;
         
+        int j = -1;
+        for(int i = 0; i < n; i++){ // finding first non zero element index. 
+            if(arr[i] == 0) {    // time complexity: O(x)
+                j = i;
+                break;
+            }
+        }
+
+        if(j == -1) {
+            System.out.println("There is no zero element the array.");
+        }
+
+        for(int i = j + 1; i < n; i++) { 
+            if(arr[i] != 0) {
+                //swap the elements of the array with jth index element 
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                j++;
+            }// Time COmplexity: O(n-x)
+        }
+        System.out.println(Arrays.toString(arr));
+        // Total time complexity: O(x) + O(n-x) = O(n)
     }
 
     @Override
@@ -55,7 +79,7 @@ public class MoveZerosToEnd  extends AlgorithmTemplate{
             arr[i] = sc.nextInt();
         }
         
-        bruteforce(arr);
+        optimal(arr);
     }
     
 }
